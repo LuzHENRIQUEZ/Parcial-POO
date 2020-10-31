5c1adad9650d40a2a660efbbf5b6fe25563dacea
Clarita <luzhenriquez2018@gmail.com>
2020-10-31 15:46:04 -0600

    Subiendo archivos

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+    $conn = mysqli_connect('localhost','root','74353051','crud_tareas');
+    /*if(isset($conn)){
+        echo "Conexion exitosa";
+    }else{
+        echo "ERROr";
+    }*/
+
+?>
---
+++ variables.py
@@ -0,0 +1,7 @@
+#primer ejercicio con variables
+#session4
+#desarrolla por Francisco Quezada
+ Nombre= "Bienvenido a la Seccion 4 de python"
+ print(Nombre)
+ """
+ Investiguen 
